0ec1d204e45ba8a447bbe063c220e8c81f7b7cd9
qinleqi <qinleqi_nt@163.com>
2025-11-12 19:32:48 +0800

    display money

--- codes/SurvivorGame.cpp
+++ codes/SurvivorGame.cpp
@@ -673,6 +673,15 @@ void SurvivorGame::drawHUD()
         foodGaugeText->setPos(10, 160);
         foodGaugeText->setZValue(10);
         scene->addItem(foodGaugeText);
+
+        //绘制金币数
+        QGraphicsTextItem *moneyText = new QGraphicsTextItem();
+        moneyText->setPlainText("金币: " + QString::number(player->getMoney()));
+        moneyText->setDefaultTextColor(Qt::yellow);
+        moneyText->setFont(QFont("Arial", 16));
+        moneyText->setPos(10, 190);
+        moneyText->setZValue(10);
+        scene->addItem(moneyText);
     } else if (currentMapId == 2) {
         // 第二张地图：重新创建 mapHint（避免被误删后无提示）
         mapHint = new QGraphicsTextItem("这是第二张地图\n移动到底部传送门按Enter返回第一张地图");
@@ -681,6 +690,14 @@ void SurvivorGame::drawHUD()
         mapHint->setPos(GAME_WIDTH / 2 - mapHint->boundingRect().width() / 2, 20);
         mapHint->setZValue(100);
         scene->addItem(mapHint);
+    } else if (currentMapId == 4) {
+        QGraphicsTextItem *moneyText = new QGraphicsTextItem();
+        moneyText->setPlainText("金币: " + QString::number(player->getMoney()));
+        moneyText->setDefaultTextColor(Qt::yellow);
+        moneyText->setFont(QFont("Arial", 16));
+        moneyText->setPos(10, 10);
+        moneyText->setZValue(10);
+        scene->addItem(moneyText);
     }
 }
 
