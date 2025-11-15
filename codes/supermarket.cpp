b312bc5b268c20e1c22e38e0909d47ac39a819ee
qinleqi <qinleqi_nt@163.com>
2025-11-15 19:57:56 +0800

    buy food and ammo in supermarket

--- codes/supermarket.cpp
+++ codes/supermarket.cpp
@@ -11,3 +11,23 @@ Supermarket::~Supermarket(){}
 QString Supermarket::update (Player *player){}
 
 QString Supermarket::applyeffect(Player *player){}
+
+bool Supermarket::buyFood(Player *player)
+{
+    if (player->getMoney() >= 10) {
+        player->setMoney(player->getMoney() - 10);
+        player->setFoodGauge(qMin(100.0f, player->getFoodGauge() + 15));
+        return true;
+    }
+    return false;
+}
+
+bool Supermarket::buyBullet(Player *player)
+{
+    if (player->getMoney() >= 20) {
+        player->setMoney(player->getMoney() - 20);
+        player->setAmmo(player->getAmmo() + 15);
+        return true;
+    }
+    return false;
+}
