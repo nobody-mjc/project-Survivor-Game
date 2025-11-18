e9a2e3b7f62237db12318029203056bb4cee6c7c
nodody <17660171086@163.com>
2025-11-18 20:17:00 +0800

    start window

---
+++ codes/menuwindow.h
@@ -0,0 +1,23 @@
+#ifndef MENUWINDOW_H
+#define MENUWINDOW_H
+
+#include <QMainWindow>
+#include <QWidget>
+
+class MenuWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MenuWindow(QWidget *parent = nullptr);
+    ~MenuWindow();
+private:
+    // 开始新游戏
+    void onStartNewGame();
+    // 读取存档
+    void onLoadGame();
+    // 退出游戏
+    void onExitGame();
+    void initUI();
+};
+
+#endif // MENUWINDOW_H
